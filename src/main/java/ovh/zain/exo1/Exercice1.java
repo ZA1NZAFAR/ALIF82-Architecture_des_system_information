510098272057f082365c25f601464e32b2e2646b
Ousseynou <osakho62@gmail.com>
2023-02-03 12:06:21 +0100

    exo1

---
+++ src/main/java/ovh/zain/exo1/Exercice1.java
@@ -0,0 +1,54 @@
+package ovh.zain.exo1;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerFactory;
+import org.apache.activemq.broker.BrokerService;
+
+public class Exercice1 {
+    public static void main(String[] args) throws URISyntaxException, Exception {
+        BrokerService broker = BrokerFactory.createBroker(new URI(
+                "broker:(tcp://localhost:61616)"));
+        broker.start();
+        Connection connection = null;
+        try {
+            // Producer
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+                    "tcp://localhost:61616");
+            connection = connectionFactory.createConnection();
+            Session session = connection.createSession(false,
+                    Session.AUTO_ACKNOWLEDGE);
+            Queue queue = session.createQueue("customerQueue");
+            String payload = "Important Task";
+            Message msg = session.createTextMessage(payload);
+            MessageProducer producer = session.createProducer(queue);
+            System.out.println("Sending text '" + payload + "'");
+            producer.send(msg);
+
+            // Consumer
+            MessageConsumer consumer = session.createConsumer(queue);
+            connection.start();
+            TextMessage textMsg = (TextMessage) consumer.receive();
+            System.out.println(textMsg);
+            System.out.println("Received: " + textMsg.getText());
+            session.close();
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            broker.stop();
+        }
+    }
+
+}
